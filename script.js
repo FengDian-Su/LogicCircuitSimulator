e1c3d52ce2d67ccfec7a7e0fb4cba4dfc0079b16
NJ <129062942+FengDian-Su@users.noreply.github.com>
2024-01-07 01:00:57 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,103 @@
+var lineCount = 0;
+
+function connectPoints() {
+    var point1Id = document.getElementById('point1Input').value;
+    var point2Id = document.getElementById('point2Input').value;
+
+    var point1 = document.getElementById(point1Id);
+    var point2 = document.getElementById(point2Id);
+
+    //alert(point1Id + ", " + point2Id);
+
+    if (!point1 || !point2) {
+        alert('Invalid point IDs');
+        return;
+    }
+
+    lineCount += 1;
+
+    var line = document.createElement('div');
+    line.className = 'line';
+    line.id = point1Id+point2Id;
+
+    //alert("line_id = "+line.id);
+
+    var x1 = point1.offsetLeft + point1.clientWidth / 2;
+    var y1 = point1.offsetTop + point1.clientHeight / 2;
+    var x2 = point2.offsetLeft + point2.clientWidth / 2;
+    var y2 = point2.offsetTop + point2.clientHeight / 2;
+
+    alert("point1:("+x1+","+y1+")"+" "+"point2:("+x2+","+y2+")");
+
+    var deltaX = x2 - x1;
+    var deltaY = y2 - y1;
+
+    var length = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+    var angle = Math.atan2(deltaY, deltaX) * (180 / Math.PI);
+
+    line.style.width = length + 'px';
+    line.style.transform = 'rotate(' + angle + 'deg)';
+    line.style.left = x1 + 'px';
+    line.style.top = y1 + 'px';
+
+    document.body.appendChild(line);
+}
+function deletePoints(){
+    var point3Id = document.getElementById('point3Input').value;
+    var point4Id = document.getElementById('point4Input').value;
+
+    var point3 = document.getElementById(point3Id);
+    var point4 = document.getElementById(point4Id);
+
+    //alert(point3Id + ", " + point4Id);
+
+    if (!point3 || !point4) {
+        alert('Invalid point IDs');
+        return;
+    }
+
+    lineCount -= 1;
+
+    var line = document.getElementById(point3Id+point4Id);
+    document.body.removeChild(line);
+}
+function createTTL() {
+    var point5Id = document.getElementById('point5Input').value;
+    var point6Id = document.getElementById('point6Input').value;
+
+    var point5 = document.getElementById(point5Id);
+    var point6 = document.getElementById(point6Id);
+
+    if (!point5 || !point6) {
+        alert('Invalid point IDs');
+        return;
+    }
+
+    var line = document.createElement('div');
+    line.className = 'line';
+    line.id = point5Id+point6Id;
+
+    var x1 = point5.offsetLeft + point5.clientWidth / 2;
+    var y1 = point5.offsetTop + point5.clientHeight / 2;
+    var x2 = point6.offsetLeft + point6.clientWidth / 2;
+    var y2 = point6.offsetTop + point6.clientHeight / 2;
+
+    alert("point1:("+x1+","+y1+")"+" "+"point2:("+x2+","+y2+")");
+
+    var deltaX = x2 - x1;
+    var deltaY = y2 - y1;
+
+    alert("width:"+deltaX+" , "+"height:"+deltaY);
+    //var length = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+
+    line.style.width = deltaX + 'px';
+    line.style.height = deltaY + 'px';
+    line.style.left = x1 + 'px';
+    line.style.top = y1 + 'px';
+
+    document.body.appendChild(line);
+}
+/*
+point1:(56.5,134.5) point2:(119.5,166.5)
+width:63 , height:32
+*/
